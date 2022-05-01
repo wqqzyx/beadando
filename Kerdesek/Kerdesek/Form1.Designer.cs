4fe785f8c1d02355740e47353198b708b2d127bc
wqqzyx <104459365+wqqzyx@users.noreply.github.com>
2022-05-01 17:19:27 +0200

    Add files via upload

--- Kerdesek/Kerdesek/Form1.Designer.cs
+++ Kerdesek/Kerdesek/Form1.Designer.cs
@@ -31,7 +31,6 @@ namespace Kerdesek {
             this.sfd = new System.Windows.Forms.SaveFileDialog();
             this.label16 = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
-            this.lbAdatok = new System.Windows.Forms.ListBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -43,18 +42,25 @@ namespace Kerdesek {
             this.tbC = new System.Windows.Forms.TextBox();
             this.tbB = new System.Windows.Forms.TextBox();
             this.tbA = new System.Windows.Forms.TextBox();
-            this.tbKerdes = new System.Windows.Forms.TextBox();
             this.bBekuld = new System.Windows.Forms.Button();
             this.LPont = new System.Windows.Forms.Label();
             this.tbAdd = new System.Windows.Forms.Label();
             this.tbNev = new System.Windows.Forms.TextBox();
             this.tbNevH = new System.Windows.Forms.Button();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.LKerdes = new System.Windows.Forms.Label();
+            this.lbAdatok = new System.Windows.Forms.ListBox();
+            this.bSegítség = new System.Windows.Forms.Button();
+            this.Penz = new System.Windows.Forms.ListBox();
+            this.tel = new System.Windows.Forms.Button();
+            this.koz = new System.Windows.Forms.Button();
+            this.fel = new System.Windows.Forms.Button();
+            this.TelHelp = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // bHozzaad
             // 
-            this.bHozzaad.Location = new System.Drawing.Point(282, 298);
+            this.bHozzaad.Location = new System.Drawing.Point(64, 366);
             this.bHozzaad.Name = "bHozzaad";
             this.bHozzaad.Size = new System.Drawing.Size(198, 23);
             this.bHozzaad.TabIndex = 12;
@@ -64,7 +70,7 @@ namespace Kerdesek {
             // 
             // bMentes
             // 
-            this.bMentes.Location = new System.Drawing.Point(333, 241);
+            this.bMentes.Location = new System.Drawing.Point(268, 366);
             this.bMentes.Name = "bMentes";
             this.bMentes.Size = new System.Drawing.Size(111, 23);
             this.bMentes.TabIndex = 15;
@@ -92,15 +98,6 @@ namespace Kerdesek {
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(0, 13);
             this.label17.TabIndex = 30;
-            // 
-            // lbAdatok
-            // 
-            this.lbAdatok.FormattingEnabled = true;
-            this.lbAdatok.Location = new System.Drawing.Point(486, 261);
-            this.lbAdatok.Name = "lbAdatok";
-            this.lbAdatok.Size = new System.Drawing.Size(234, 147);
-            this.lbAdatok.TabIndex = 13;
-            this.lbAdatok.SelectedIndexChanged += new System.EventHandler(this.lbAdatok_SelectedIndexChanged);
             // 
             // label5
             // 
@@ -132,7 +129,7 @@ namespace Kerdesek {
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(409, 366);
+            this.label1.Location = new System.Drawing.Point(409, 371);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 6;
@@ -195,15 +192,6 @@ namespace Kerdesek {
             this.tbA.TabIndex = 5;
             this.tbA.Text = "A";
             // 
-            // tbKerdes
-            // 
-            this.tbKerdes.Location = new System.Drawing.Point(268, 12);
-            this.tbKerdes.Multiline = true;
-            this.tbKerdes.Name = "tbKerdes";
-            this.tbKerdes.Size = new System.Drawing.Size(270, 39);
-            this.tbKerdes.TabIndex = 0;
-            this.tbKerdes.Text = "Kérdés";
-            // 
             // bBekuld
             // 
             this.bBekuld.Location = new System.Drawing.Point(503, 139);
@@ -225,7 +213,7 @@ namespace Kerdesek {
             // tbAdd
             // 
             this.tbAdd.AutoSize = true;
-            this.tbAdd.Location = new System.Drawing.Point(265, 106);
+            this.tbAdd.Location = new System.Drawing.Point(265, 109);
             this.tbAdd.Name = "tbAdd";
             this.tbAdd.Size = new System.Drawing.Size(94, 13);
             this.tbAdd.TabIndex = 33;
@@ -253,11 +241,90 @@ namespace Kerdesek {
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
             // 
+            // LKerdes
+            // 
+            this.LKerdes.Location = new System.Drawing.Point(166, 21);
+            this.LKerdes.Name = "LKerdes";
+            this.LKerdes.Size = new System.Drawing.Size(500, 13);
+            this.LKerdes.TabIndex = 36;
+            this.LKerdes.Text = "Kérdés";
+            // 
+            // lbAdatok
+            // 
+            this.lbAdatok.FormattingEnabled = true;
+            this.lbAdatok.Location = new System.Drawing.Point(657, 365);
+            this.lbAdatok.Name = "lbAdatok";
+            this.lbAdatok.Size = new System.Drawing.Size(133, 56);
+            this.lbAdatok.TabIndex = 13;
+            this.lbAdatok.SelectedIndexChanged += new System.EventHandler(this.lbAdatok_SelectedIndexChanged);
+            // 
+            // bSegítség
+            // 
+            this.bSegítség.Location = new System.Drawing.Point(504, 172);
+            this.bSegítség.Name = "bSegítség";
+            this.bSegítség.Size = new System.Drawing.Size(134, 23);
+            this.bSegítség.TabIndex = 37;
+            this.bSegítség.Text = "Segitségkérés";
+            this.bSegítség.UseVisualStyleBackColor = true;
+            this.bSegítség.Click += new System.EventHandler(this.bSegítség_Click);
+            // 
+            // Penz
+            // 
+            this.Penz.FormattingEnabled = true;
+            this.Penz.Location = new System.Drawing.Point(657, 106);
+            this.Penz.Name = "Penz";
+            this.Penz.Size = new System.Drawing.Size(133, 212);
+            this.Penz.TabIndex = 38;
+            // 
+            // tel
+            // 
+            this.tel.Location = new System.Drawing.Point(503, 211);
+            this.tel.Name = "tel";
+            this.tel.Size = new System.Drawing.Size(135, 23);
+            this.tel.TabIndex = 39;
+            this.tel.Text = "Telefon";
+            this.tel.UseVisualStyleBackColor = true;
+            this.tel.Click += new System.EventHandler(this.tel_Click);
+            // 
+            // koz
+            // 
+            this.koz.Location = new System.Drawing.Point(504, 240);
+            this.koz.Name = "koz";
+            this.koz.Size = new System.Drawing.Size(135, 23);
+            this.koz.TabIndex = 40;
+            this.koz.Text = "Közönség";
+            this.koz.UseVisualStyleBackColor = true;
+            this.koz.Click += new System.EventHandler(this.koz_Click);
+            // 
+            // fel
+            // 
+            this.fel.Location = new System.Drawing.Point(504, 269);
+            this.fel.Name = "fel";
+            this.fel.Size = new System.Drawing.Size(135, 23);
+            this.fel.TabIndex = 41;
+            this.fel.Text = "Felezés";
+            this.fel.UseVisualStyleBackColor = true;
+            // 
+            // TelHelp
+            // 
+            this.TelHelp.AutoSize = true;
+            this.TelHelp.Location = new System.Drawing.Point(64, 269);
+            this.TelHelp.Name = "TelHelp";
+            this.TelHelp.Size = new System.Drawing.Size(0, 13);
+            this.TelHelp.TabIndex = 42;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(820, 433);
+            this.Controls.Add(this.TelHelp);
+            this.Controls.Add(this.fel);
+            this.Controls.Add(this.koz);
+            this.Controls.Add(this.tel);
+            this.Controls.Add(this.Penz);
+            this.Controls.Add(this.bSegítség);
+            this.Controls.Add(this.LKerdes);
             this.Controls.Add(this.tbNevH);
             this.Controls.Add(this.tbNev);
             this.Controls.Add(this.tbAdd);
@@ -279,7 +346,6 @@ namespace Kerdesek {
             this.Controls.Add(this.tbC);
             this.Controls.Add(this.tbD);
             this.Controls.Add(this.tbHelyes);
-            this.Controls.Add(this.tbKerdes);
             this.Name = "Form1";
             this.Text = "Kérdések";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -295,7 +361,6 @@ namespace Kerdesek {
         private System.Windows.Forms.SaveFileDialog sfd;
         private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Label label17;
-        private System.Windows.Forms.ListBox lbAdatok;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -307,13 +372,20 @@ namespace Kerdesek {
         private System.Windows.Forms.TextBox tbC;
         private System.Windows.Forms.TextBox tbB;
         private System.Windows.Forms.TextBox tbA;
-        private System.Windows.Forms.TextBox tbKerdes;
         private System.Windows.Forms.Button bBekuld;
         private System.Windows.Forms.Label LPont;
         private System.Windows.Forms.Label tbAdd;
         private System.Windows.Forms.TextBox tbNev;
         private System.Windows.Forms.Button tbNevH;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label LKerdes;
+        private System.Windows.Forms.ListBox lbAdatok;
+        private System.Windows.Forms.Button bSegítség;
+        private System.Windows.Forms.ListBox Penz;
+        private System.Windows.Forms.Button tel;
+        private System.Windows.Forms.Button koz;
+        private System.Windows.Forms.Button fel;
+        private System.Windows.Forms.Label TelHelp;
     }
 }
 
--- Kerdesek/Kerdesek/Form1.cs
+++ Kerdesek/Kerdesek/Form1.cs
@@ -21,6 +21,9 @@ namespace Kerdesek {
         string nev = "";
         int[] nyeremenyek = new int[] { 1000, 3000, 6000, 12000, 25000, 50000, 100000, 200000, 400000, 800000, 1500000, 3000000, 6000000, 12500000, 25000000 };
         int elnyertPenz = 0;
+        bool kozonseg = false;
+        bool telefon = false;
+        bool felezes = false;
         public void bMegnyitas_Click(object sender, EventArgs e) {
             
             
@@ -37,7 +40,7 @@ namespace Kerdesek {
 
         private void lbAdatok_SelectedIndexChanged(object sender, EventArgs e) {
             int index = lbAdatok.SelectedIndex;
-            tbKerdes.Text = a.kerdesek[index].kerdes;
+            LKerdes.Text = a.kerdesek[index].kerdes;
             tbA.Text = a.kerdesek[index].a;
             tbB.Text = a.kerdesek[index].b;
             tbC.Text = a.kerdesek[index].c;
@@ -47,7 +50,7 @@ namespace Kerdesek {
 
         private void bHozzaad_Click(object sender, EventArgs e) {
             Kerdes k = new Kerdes();
-            k.kerdes = tbKerdes.Text;
+            k.kerdes = LKerdes.Text;
             k.a = tbA.Text;
             k.b = tbB.Text;
             k.c = tbC.Text;
@@ -72,10 +75,16 @@ namespace Kerdesek {
                     k.helyesValasz = Convert.ToChar(adatok[5]);
                     a.kerdesek.Add(k);
                     //lbAdatok.Items.Add(k.kerdes);
-                    
                 }
+            for (int i = 0; i < nyeremenyek.Length; i++)
+            {
+                Penz.Items.Add(nyeremenyek[i]+" Ft");
+            }
                 sr.Close();
             gen();
+            tel.Visible = false;
+            koz.Visible = false;
+            fel.Visible = false;
             //}
         }
         public void gen()
@@ -83,7 +92,8 @@ namespace Kerdesek {
             Random r = new Random();
             v = r.Next(0, a.kerdesek.Count);
             Kerdes kerdes = a.kerdesek[v];
-            tbKerdes.Text = kerdes.kerdes;
+            LKerdes.Text = kerdes.kerdes;
+            LKerdes.TextAlign = ContentAlignment.TopCenter;
             tbA.Text = kerdes.a;
             tbB.Text = kerdes.b;
             tbC.Text = kerdes.c;
@@ -106,7 +116,7 @@ namespace Kerdesek {
             {
                 pont++;
                 LPont.Text = pont.ToString();
-                k++;
+                
                 elnyertPenz = nyeremenyek[k];
                 nyeremenyek[k] = nyeremenyek[k++];
                 lbAdatok.Items.Add(elnyertPenz);
@@ -118,8 +128,8 @@ namespace Kerdesek {
                 LPont.Text = pont.ToString();
                 gen();
                 tbHelyes.Text = "";
-                string xd=elnyertPenz.ToString();
-                    MessageBox.Show("Válaszod helytelen, így kiestél a játékból, nyereményed pedig:","RIPBOZO");
+                
+                    MessageBox.Show("Válaszod helytelen, így kiestél a játékból, nyereményed pedig: {0}");
                 } 
             
             } 
@@ -138,6 +148,46 @@ namespace Kerdesek {
             tbV.Location=new Point(265,106);
             tbHelyes.Location = new Point(320, 106);
             bBekuld.Location = new Point(490, 105);
+        }
+
+        private void bSegítség_Click(object sender, EventArgs e)
+        {
+            bSegítség.Visible = false;
+            tel.Visible = true;
+            koz.Visible = true;
+            fel.Visible = true;
+        }
+
+        private void tel_Click(object sender, EventArgs e)
+        {
+            if (telefon==false)
+            {
+            int random;
+            Random r = new Random();
+            random=r.Next(0, 3);
+            TelHelp.Text = "Jóska bácsi szerint a helyes megoldás a: " + random;
+            telefon = true;
+            } 
+            else
+            {
+                MessageBox.Show( "Használtad már ezt a segítséget","Hiba",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void koz_Click(object sender, EventArgs e)
+        {
+            if (kozonseg == false)
+            {
+                int random;
+                Random r = new Random();
+                random = r.Next(0, 3);
+                TelHelp.Text = "A közönség szavazatai szerint a helyes megoldás a: " + random;
+                kozonseg = true;
+            }
+            else
+            {
+                MessageBox.Show("Használtad már ezt a segítséget", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
